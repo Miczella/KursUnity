5c6af8fa7b9b80cb06dc91dec72d32613e503a2f
Miczella <j.justyna12@o2.pl>
2018-02-09 19:01:37 +0100

    Tak

---
+++ Assets/2D Platformer Controller/Scripts/Ruch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruch : MonoBehaviour {
+	public Rigidbody2D Fizyka;
+	public float MocSkoku;
+	public float SzybkoscPoruszaniasie;
+    public bool jestnaziemi;
+	// Use this for initialization
+	void Start () {
+		Fizyka = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")&&jestnaziemi == true) {
+			Fizyka.AddForce (Vector2.up*MocSkoku, ForceMode2D.Impulse);
+		}
+		if (Input.GetAxis ("Horizontal")!=0 && jestnaziemi == true) {
+			Fizyka.AddForce (Vector2.right * SzybkoscPoruszaniasie*Input.GetAxis ("Horizontal"), ForceMode2D.Force);
+		}
+	}
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.tag == "Ziemia")
+        {
+            jestnaziemi = true;
+        }
+    }
+	void OnCollisionStay2D (Collision2D col)
+	{
+		if (col.gameObject.tag == "Ziemia")
+		{
+			jestnaziemi = true;
+		}
+	}
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ziemia")
+        {
+            jestnaziemi = false;
+        }
+    }
+}
